955e61df4d60a018087d339f1756a505706d2e49
xtechsouthie <sohamgawand2014@gmail.com>
2025-12-26 23:56:58 +0530

    improved utils to maintain architectural similarities between BC and RL phase

--- utils.py
+++ utils.py
@@ -8,8 +8,11 @@ import random
 from torch_geometric.data import Data
 
 base_list = ['A', 'U', 'C', 'G']
+base_to_idx = {'A': 1, 'U': 2, 'C': 3, 'G': 4}
+structure_to_idx = {'.': 1, '(': 2, ')': 3}
 onehot_list = [np.array([1, 0, 0, 0]), np.array([0, 1, 0, 0]), np.array([0, 0, 1, 0]), np.array([0, 0, 0, 1])]
-
+base_pair_list_4 = [['A', 'U'], ['U', 'A'], ['C', 'G'], ['G', 'C']]
+base_pair_list_6 = [['A', 'U'], ['U', 'A'], ['U', 'G'], ['G', 'U'], ['G', 'C'], ['C', 'G']]
 
 def structure_dotB2Edge(dotB):
     l = len(dotB)
@@ -30,6 +33,41 @@ def structure_dotB2Edge(dotB):
             v += [last_place, i]
     edge_index = torch.tensor(np.array([u, v]))
     return edge_index
+
+def get_pairing_partner(dotB: str):
+    l = len(dotB)
+
+    partners = np.full(l, -1, dtype=np.float32)
+    stack = []
+
+    for i, char in enumerate(dotB):
+        if char == "(":
+            stack.append(i)
+        elif char == ")":
+            j = stack.pop()
+            partners[i] = j
+            partners[j] = i
+
+    return partners
+
+def get_pair_indices_from_dotB(dotB: str):
+    pairs = []
+    stack = []
+
+    for i, char in enumerate(dotB):
+        if char == '(':
+            stack.append(i)
+        elif char == ')':
+            j = stack.pop()
+            pairs.append((j, i))
+
+    return pairs
+
+def encode_structure(dotB: str):
+    return np.array([structure_to_idx.get(char, 1) for char in dotB])
+
+def encode_sequence(seq_base: str):
+    return np.array([base_to_idx.get(base, 1) for base in seq_base]) 
 
 def decode_structure(encoded: np.ndarray):
 
@@ -192,83 +230,160 @@ def simple_init_sequence(dotB, base_order, max_size=None):
     seq_onehot = torch.tensor(seq_onehot)
     return seq_base, seq_onehot
 
+def build_node_features_8ch(seq_base: str, current_structure: str, 
+                            target_structure: str, current_energy: float, target_energy: float):
+    
+    seq_len = len(seq_base)
+
+    ch0 = encode_sequence(seq_base)
+    ch1 = encode_structure(current_structure)
+    ch2 = encode_structure(target_structure)
+    ch3 = np.full(seq_len, current_energy, dtype=np.float32)
+    ch4 = np.full(seq_len, target_energy, dtype=np.float32)
+    ch5 = get_pairing_partner(current_structure)
+    ch6 = get_pairing_partner(target_structure)
+    ch7 = np.ones(seq_len, dtype=np.float32)
+
+    features = np.stack([ch0, ch1, ch2, ch3, ch4, ch5, ch6, ch7], axis=0)
+    features = torch.Tensor(features.T, dtype=torch.float32)
+
+    return features    
+
+def random_base_pair(action_space: int):
+    pair_list = base_pair_list_4 if action_space == 4 else base_pair_list_6
+    idx = random.randrange(0, len(pair_list))
+    return pair_list[idx][0], pair_list[idx][1]
+
+
+def random_init_sequence_pair(dotB: str, max_size: int, action_space: int = 4):
+
+    l = len(dotB)
+    seq_list = [''] * l
+
+    pairs = get_pair_indices_from_dotB(dotB)
+    paired_positions = set()
+
+    for i, j in pairs:
+        base_i, base_j = random_base_pair(action_space)
+        seq_list[i] = base_i
+        seq_list[j] = base_j
+        paired_positions.add(i)
+        paired_positions.add(j)
+
+    for i in range(l):
+        if i not in paired_positions:
+            seq_list[i], _ = random_base()
+
+    seq_base = ''.join(seq_list)
+    seq_onehot = seq_base2Onehot(seq_base, max_size)
+
+    return seq_base, seq_onehot
+
 
 def get_graph(dotB=None, max_size=None, seq_base=None, seq_onehot=None, edge_threshold=0.001):
         
     if max_size is None:
-        max_size = len(seq_base if seq_base is not None else dotB)
-    if seq_base == None:
-        seq_base, seq_onehot = random_init_sequence(dotB, max_size)
-    else:
-        if seq_onehot is None:
-            seq_onehot = seq_base2Onehot(seq_base)
+        max_size = len(dotB)
 
     seq_length = len(dotB)
-    node_features = seq_onehot
-
-    edge_index = structure_dotB2Edge(dotB)
-
-    num_edges = edge_index.size(1)
-    edge_attr = torch.ones((num_edges, 1), dtype=torch.float)
+    
+    if seq_base is None:
+        seq_base, seq_onehot = random_init_sequence_pair(dotB, max_size)
+    elif seq_onehot is None:
+        seq_onehot = seq_base2Onehot(seq_base, max_size)
 
     real_structure, real_energy = RNA.fold(seq_base)
+
+    target_energy = get_energy_from_base(seq_base, dotB)
+
+    node_features = build_node_features_8ch(
+        seq_base = seq_base,
+        current_structure= real_structure,
+        target_structure= dotB,
+        current_energy= real_energy,
+        target_energy= target_energy
+    )
+
+    aim_edge_index = structure_dotB2Edge(dotB)
+    real_edge_index = structure_dotB2Edge(real_structure)
+
+    edge_index, edge_attr = build_edge_index_attr(real_structure, seq_length)
+
     distance = get_distance_from_base(seq_base, dotB)
 
     single_indices = [i for i, char in enumerate(dotB) if char == "."]
-
-    stack = []
-    pair_indices = []
-    for i, char in enumerate(dotB):
-        if char == "(":
-            pair_indices.append(i)
-            stack.append(i)
-        elif char == ")":
-            h = stack.pop()
-
-    aim_adj = torch.zeros((seq_length, seq_length), dtype=torch.float)
-    real_adj = torch.zeros((seq_length, seq_length), dtype=torch.float)
-
-    stack = []
-    for i, char in enumerate(dotB):
-        if char == '(':
-            stack.append(i)
-        elif char == ')':
-            if stack:
-                j = stack.pop()
-                aim_adj[i, j] = 1
-                aim_adj[j, i] = 1
-    
-    stack = []
-    for i, char in enumerate(real_structure):
-        if char == '(':
-            stack.append(i)
-        elif char == ')':
-            if stack:
-                j = stack.pop()
-                real_adj[i, j] = 1
-                real_adj[j, i] = 1
+    pair_indices = [i for i, char in enumerate(dotB) if char == "("]
 
     y_dict = {
         'dotB': dotB,
         'seq_base': seq_base,
         'real_dotB': real_structure,
-        'energy': real_energy,
+        'aim_energy': target_energy,
+        'real_energy': real_energy,
         'distance': distance,
-        'single_index': torch.tensor(single_indices, dtype=torch.long),
-        'pair_index': torch.tensor(pair_indices, dtype=torch.long),
-        'aim_adj': aim_adj,
-        'real_adj': real_adj,
-        'length': seq_length,
+        'single_index': single_indices,
+        'pair_index': pair_indices,
+        'aim_edge_index': aim_edge_index,
+        'real_edge_index': real_edge_index,
+        'length': seq_length
     }
 
     graph = Data(
-        x=node_features,
-        edge_index=edge_index,
-        edge_attr=edge_attr, # could make it more informative like edge_attr.append([is_backbone, is_pair, 1.0, dist])
-        y=y_dict
+        x = node_features,
+        edge_index= edge_index,
+        edge_attr = edge_attr,
+        y= y_dict
     )
 
     return graph
+
+def update_graph_after_mutation(graph: Data, new_seq: str, target_dotB: str):
+    seq_length = len(new_seq)
+
+    real_structure, real_energy = RNA.fold(new_seq)
+    target_energy = get_energy_from_base(new_seq, target_dotB)
+
+    node_features = build_node_features_8ch(
+        seq_base= new_seq,
+        current_structure= real_structure, 
+        target_structure= target_dotB,
+        current_energy= real_energy,
+        target_energy= target_energy
+    )
+
+    edge_index, edge_attr = build_edge_index_attr(real_structure, seq_length)
+
+    distance = get_distance_from_base(new_seq, target_dotB)
+
+    real_edge_index = structure_dotB2Edge(real_structure)
+    
+    graph.x = node_features
+    graph.edge_index = edge_index
+    graph.edge_attr = edge_attr
+
+    graph.y['seq_base'] = new_seq
+    graph.y['real_dotB'] = real_structure
+    graph.y['aim_energy'] = target_energy
+    graph.y['real_energy'] = real_energy
+    graph.y['distance'] = distance
+    graph.y['real_edge_index'] = real_edge_index
+
+    return graph
+
+
+def rna_act(graph: Data, location: int, mutation: int, action_space: int = 4):
+
+    seq_list = list(graph.y['seq_base'])
+    target_dotB = graph.y['dotB']
+
+    seq_list[location] = base_list[mutation]
+
+    new_seq = ''.join(seq_list)
+
+    graph = update_graph_after_mutation(graph, new_seq, target_dotB)
+
+    return graph
+
 
 def simplify_graph(graph):
     simplified = Data(
